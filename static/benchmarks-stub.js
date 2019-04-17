e5e253c896c14b4cc46a8b201a52a543922cd178
Pierre <pierre.burton91@gmail.com>
2019-04-17 15:55:00 +0200

    format

--- static/benchmarks-stub.js
+++ static/benchmarks-stub.js
@@ -1,12 +1,1 @@
-export default dataStub = [
-    {title: 'Grace', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-    {title: 'Hello', valuesTypesKey: 0, text: 'My first item', value: '110 Kg', date: '01-01-2018'},
-]
+export default dataStub = []
--- views/benchmarks/benchmarks-screen.js
+++ views/benchmarks/benchmarks-screen.js
@@ -14,87 +14,87 @@ export default class BenchmarksScreen extends React.Component {
     super(props);
 
     this.state = {
-        scrollY: new Animated.Value(0),
-        data: [],
-        showAd: true
+      scrollY: new Animated.Value(0),
+      data: [],
+      showAd: true
     }
 
     this._handleRecordSelect = this._handleRecordSelect.bind(this);
     this._handleDeleteRecord = this._handleDeleteRecord.bind(this);
-}
+  }
 
-componentDidMount() {
+  componentDidMount() {
     this.setState({ data: dataStub })
-}
+  }
 
-_handleRecordSelect(element) {
-    this.props.navigation.navigate('Details', {record: element});
-}
+  _handleRecordSelect(element) {
+    this.props.navigation.navigate('Details', { record: element });
+  }
 
-_handleDeleteRecord(record) {
-    Alert.alert('Confirm deletion', 'This action will delete the record and all historical data associated.\nIt can\'t be undone.', [{text: 'Confirm', onPress: () => this._deleteRecord(record)}, {text: 'Cancel', style: 'cancel'}], {cancelable: true});
-}
+  _handleDeleteRecord(record) {
+    Alert.alert('Confirm deletion', 'This action will delete the record and all historical data associated.\nIt can\'t be undone.', [{ text: 'Confirm', onPress: () => this._deleteRecord(record) }, { text: 'Cancel', style: 'cancel' }], { cancelable: true });
+  }
 
-_deleteRecord(record) {
+  _deleteRecord(record) {
     const index = this.state.data.indexOf(record);
     const temp = this.state.data.slice();
     temp.splice(index, 1);
-    this.setState({data: temp});
+    this.setState({ data: temp });
     this.state.scrollY.setValue(0);
-}
+  }
 
-_handleNewRecord() {
-    this.props.navigation.navigate('AddRecord_newType', {command: new AddRecordCommand()});
-}
+  _handleNewRecord() {
+    this.props.navigation.navigate('AddRecord_newType', { command: new AddRecordCommand() });
+  }
 
-_renderRecords(item, index) {
-    if (index%20 == 0) {
-        return (
-            <View>
-                <View style={{ display: this.state.showAd ? 'flex' : 'none', alignItems: 'center', marginTop: 8}}>
-                    <AdMobBanner
-                        bannerSize="banner"
-                        adUnitID="ca-app-pub-3940256099942544/6300978111" // ca-app-pub-3940256099942544/6300978111
-                        testDeviceID={Constants.installationId}
-                        onDidFailToReceiveAdWithError={() => this.setState({ showAd: false })} />
-                </View>
-            <BenchmarkComponent _handleDeleteRecord={this._handleDeleteRecord} _handleRecordSelect={this._handleRecordSelect} key={index} record={item} />
-            </View>
-        )
+  _renderRecords(item, index) {
+    if (index % 20 == 0) {
+      return (
+        <View>
+          <View style={{ display: this.state.showAd ? 'flex' : 'none', alignItems: 'center', marginTop: 8 }}>
+            <AdMobBanner
+              bannerSize="banner"
+              adUnitID="ca-app-pub-3940256099942544/6300978111" // ca-app-pub-3940256099942544/6300978111
+              testDeviceID={Constants.installationId}
+              onDidFailToReceiveAdWithError={() => this.setState({ showAd: false })} />
+          </View>
+          <BenchmarkComponent _handleDeleteRecord={this._handleDeleteRecord} _handleRecordSelect={this._handleRecordSelect} key={index} record={item} />
+        </View>
+      )
     }
     return (<BenchmarkComponent _handleDeleteRecord={this._handleDeleteRecord} _handleRecordSelect={this._handleRecordSelect} key={index} record={item} />)
-}
+  }
 
-render() {
+  render() {
     const recordsList = this.state.data.slice();
     const headerHeight = this.state.scrollY.interpolate({
-        inputRange: [0, 112],
-        outputRange: [200, 88],
-        extrapolate: 'clamp'
+      inputRange: [0, 112],
+      outputRange: [200, 88],
+      extrapolate: 'clamp'
     });
 
     const headerBlur = this.state.scrollY.interpolate({
-        inputRange: [0, 112],
-        outputRange: [0 , 1],
-        extrapolate: 'clamp'
+      inputRange: [0, 112],
+      outputRange: [0, 1],
+      extrapolate: 'clamp'
     });
-    
+
     const AnimatedHeaderHero = Animated.createAnimatedComponent(HeaderHero);
     const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
     return (
-    <View style={styles.container}>
+      <View style={styles.container}>
         <AnimatedFlatList
-            contentContainerStyle={{paddingTop: 200, paddingBottom: 80}}
-            data={recordsList}
-            renderItem={({item, index}) =>
-                this._renderRecords(item, index)
-            }
-            scrollEventThrottle={0}
-            onScroll={Animated.event(
-                [{nativeEvent: {contentOffset: {y: this.state.scrollY}}}]
-            )}
-            keyExtractor={(item, index) => item + index}
+          contentContainerStyle={{ paddingTop: 200, paddingBottom: 80 }}
+          data={recordsList}
+          renderItem={({ item, index }) =>
+            this._renderRecords(item, index)
+          }
+          scrollEventThrottle={0}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: this.state.scrollY } } }]
+          )}
+          keyExtractor={(item, index) => item + index}
         />
         <AnimatedHeaderHero title={"Benchmarks"} banner={'benchmarks'} height={headerHeight} blur={headerBlur} />
         <LinearGradient start={[.5, 0]} end={[.5, 1]} colors={['transparent', 'rgba(0,0,0,.16)']} style={styles.tabBarShadow}></LinearGradient>
